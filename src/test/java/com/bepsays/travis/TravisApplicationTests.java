692c1e183d445c766ea38d8f4fb1f28d1e557277
bjornerik <bjorn.erik.pedersen@gmail.com>
2014-06-13 21:29:23 +0200

    Smoke test for the main app launcher

---
+++ src/test/java/com/bepsays/travis/TravisApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bepsays.travis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TravisApplicationTests {
+
+    @Test
+    public void startApplication() {
+        TravisApplication.main(new String[] {});
+    }
+
+
+}
